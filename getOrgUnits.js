0695e3e259944f0d1bb67105fc5638acba9efdf1
zubaira <zubair@dhis2.org>
2023-10-12 11:06:33 +0200

    updated

--- getOrgUnits.js
+++ getOrgUnits.js
@@ -1,14 +1,21 @@
 'use strict';
+
+const propertiesReader = require('properties-reader');
 const http = require('https');
+const fileSystem = require('fs');
 const Client = require('node-rest-client').Client;  
 const client = new Client();
 
-const URL = "https://demo.pakdhis2.org/api/organisationUnitGroups/QSKdbtmrMj9.json";
+const properies = loadProperties() ;
+
+const URL = properies.get('server.url');
+const API_TOKEN = 'ApiToken ' + properies.get('server.token');
+
 
 let getRequestArgs = {
     headers: { 
         "Content-Type": "application/json",
-        "Authorization": "ApiToken <token>",
+        "Authorization": `ApiToken ${API_TOKEN}`,
         "Accept":"application/json"
     }
 };
@@ -34,3 +41,15 @@ let startDownload = async () => {
 }
 
 startDownload();
+
+
+function loadProperties()
+{
+    if ( !fileSystem.existsSync("properties.file")){
+        console.error('Properties file not found');
+        process.exit();
+    }
+
+    console.log('Loading properties from file');
+    return new propertiesReader("properties.file");
+}
