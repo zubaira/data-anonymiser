25ecfe357b87b4e9c6fa42d0310a778a7c50fd29
zubaira <zubair@dhis2.org>
2023-10-21 16:15:48 +0200

    load cnic from external file

--- fetch_data.mjs
+++ fetch_data.mjs
@@ -4,19 +4,7 @@ import fetch from 'node-fetch';
 import fs  from 'fs';
 import path from 'path';
 import propertiesReader from 'properties-reader';
-
-
-// Function to generate random names
-function getRandomName() {
-    const names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry", "Ivy", "Jack"];
-    const randomIndex = Math.floor(Math.random() * names.length);
-    return names[randomIndex];
-}
-function getRandomCnic() {
-    const cnic= ["1121212343212", "1235678901231", "7654890123619", "2589631475369", "9874563214569", "7852147963254", "7539513579515", "8965412365478", "I7852149634785", "7896541236541"];
-    const randomIndex2 = Math.floor(Math.random() * cnic.length);
-    return cnic[randomIndex2];
-}
+import { error } from 'console';
 
 const properties = loadProperties();
 const apiToken = properties.get("source.server.token");
@@ -25,6 +13,8 @@ const sourceServerOrgUnitUrl = properties.get( "source.server.orgunit.url" );
 const downloadDirectory = properties.get( "source.download.file" );
 const logging = process.argv[3];
 
+const nameCollection = getRandomName();
+const cnicCollection = getRandomCnic();
 
 // Fetch organization unit groups data with Authorization header
 async function fetchData() {
@@ -36,24 +26,27 @@ async function fetchData() {
         });
         const data = await response.json();
 
-        if ( logEnabled() ) console.log( data );
-        
         // Extract organization unit IDs from the response
         const ouIds = data.organisationUnits.map(unit => unit.id);
 
         // Process each organization unit ID
         
-        fs.mkdir(downloadDirectory, err => {
-            if(err){
-                console.error( err );
-            }else{
-                if ( logEnabled() ) console.log(" directory created " );
-            }
-        });
+        if ( fs.existsSync( downloadDirectory ) ){
+            console.log(`Directory ${downloadDirectory} exits`);
+        }else{
+            fs.mkdir(downloadDirectory, err => {
+                if(err){
+                    console.error( err );
+                }else{
+                    if ( logEnabled() ) console.log(" directory created " );
+                }
+            });
+        }
         
+         console.log("Starting download for all OrganisationUnits");
          await Promise.all(ouIds.map(async ouId => {
 
-            if ( logEnabled == '-d' ) console.log( "Downloading data from: " + ouId );
+            if ( logEnabled() ) console.log( "Downloading data for: " + ouId );
 
             const teiUrl = sourceServerOrgUnitUrl.replace( '${ouId}',ouId);
             const response = await fetch( teiUrl, {
@@ -71,9 +64,9 @@ async function fetchData() {
                     return attribute.attribute === "sB1IHYu2xQT" || attribute.attribute === "ENRjVGxVL6l"  || attribute.attribute === "z9uj8ikQFQ2";
                 }).map(attribute => {
                     if (attribute.attribute === "sB1IHYu2xQT" || attribute.attribute === "ENRjVGxVL6l") {
-                        attribute.value = getRandomName();
+                        attribute.value = nameCollection[Math.floor(Math.random() * nameCollection.length)];
                     } else if (attribute.attribute === "z9uj8ikQFQ2") {
-                        attribute.value = getRandomCnic();
+                        attribute.value = cnicCollection[Math.floor(Math.random() * cnicCollection.length)];
                     }
                     return attribute;
                 
@@ -90,20 +83,31 @@ async function fetchData() {
 
             // Write the JSON string to a file named 'output.json' with the organization unit ID as the filename
             const filePath = path.join(downloadDirectory, `${ouId}.json`);
-            await fs.writeFile(filePath, jsonString, 'utf-8', err => {
+             fs.writeFile(filePath, jsonString, 'utf-8', err => {
                  if(err)console.error( err );
                  
-                 if ( logEnabled()  )console.log(" File Written " );
+                 if ( logEnabled()  )console.log(" File Written: " + filePath );
             });
-
-            if(logEnabled())console.log(`File successfully created: ${filePath}`);
-        }));   
+        })).then( data => {
+            console.log(`Data successfully downloaded at ${downloadDirectory}`);
+        }).catch( error => {
+            console.error( "Data download failed" + error)
+        });   
     } catch (error) {
         console.error(error);
     }
 }
 
 fetchData(); // Call the function to start the process
+
+function getRandomName() {
+      var json = JSON.parse(fs.readFileSync('./names.json').toString());
+      return json; 
+}
+function getRandomCnic() {
+    var json = JSON.parse(fs.readFileSync('./cnic.json').toString());
+    return json; 
+}
 
 function loadProperties()
 {
